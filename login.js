3f275d89d958bfe6be2545757227618bb6d8bf3f
inmate302 <nelson.painkiller@gmail.com>
2026-06-14 20:18:15 -0400

    refactor(login) login.js limpieza y documentación de código

--- login.js
+++ login.js
@@ -5,11 +5,12 @@ const pwd = "1234";
 let emailvalid = false;
 let pwdvalid = false;
 
+/* función de login previa a implementación de login con Jquery
 function login() {
-    /*    
+        
     const inputemail = document.getElementById("email").value.trim().toLowerCase();
     const inputpwd = document.getElementById("pwd").value;
-   */
+   
 
     emailvalid = false;
     pwdvalid = false;
@@ -32,8 +33,9 @@ function login() {
         alert("Great Success!");
         location.replace("menu.html");
     }
-}
+}*/
 
+//validación de login con Jquery para satisfacer a Sence y a los Dioses Antiguos
 $("#form-login").on("submit", function(event) {
     event.preventDefault();
     const inputemail = $("#email").val().trim().toLowerCase();
@@ -46,29 +48,3 @@ $("#form-login").on("submit", function(event) {
         alert("Contraseña o correo incorrecto");
     }
 })
-
-/*   Intento de uso de event listener on click
-document.addEventListener("click", e => {
- if (e.target === "Depositar") {
-    let x = getElementById("redirectMsg").value; 
-    x = "Redirigiendo a Depositar";
-    alert(x);
-    setTimeout(() => { location.href("./deposit.html")}, 5000);
- }
- if (e.target === "Enviar Dinero") {
-    let x = getElementById("redirectMsg").value; 
-    x = "Redirigiendo a Enviar Dinero";
-    alert(x);
-    setTimeout(() => { location.href("./sendmoney.html")}, 5000);
-
- }
-
- if (e.target === "Últimos Movimientos") {
-    let x = getElementById("redirectMsg").value; 
-    x = "Redirigiendo a Últimos Movimientos";
-    alert(x);
-    setTimeout(() => {location.href("./transactions.html")}, 5000);
- }
-});
-
-*/
